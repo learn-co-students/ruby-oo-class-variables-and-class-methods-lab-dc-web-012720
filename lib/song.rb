008f78dd3af4a6465726215c7345f682513af6a9
github username <github email address>
2020-01-29 11:29:21 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,11 +18,11 @@ class Song
     end
 
     def self.genres
-        @@genres = @@genres.uniq
+        @@genres.uniq
     end
 
     def self.artists
-        @@artists = @@artists.uniq
+        @@artists.uniq
     end
 
     def self.genre_count
